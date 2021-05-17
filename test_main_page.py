e0609470207a108f897052cf89375bf6ae285839
maxim <2893_maxon41k@mail.ru>
2021-05-17 16:17:56 +0300

    deleted method go_to_login_page() from test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,3 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
