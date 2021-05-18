c665562cf7d8b4d5583de34e429d25dceb1676ab
maxim <2893_maxon41k@mail.ru>
2021-05-18 17:44:12 +0300

    added product_page.py

---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket_button.click()
+
+    def should_be_message_about_product_added_to_basket(self):
+        product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
+        product_added_to_basket_message = self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_MESSAGE).text
+        assert product_title in product_added_to_basket_message, f"Product {product_title} do not added to basket"
+
+    def should_be_message_basket_total(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
+        assert product_price == basket_total, "Basket cost does not match the product price"
