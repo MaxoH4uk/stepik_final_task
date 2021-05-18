46a3773d3be554bae0cd0cfa609a8414a810eb79
maxim <2893_maxon41k@mail.ru>
2021-05-18 17:43:47 +0300

    added ProductPageLocators class

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_ADDED_MESSAGE = (By.CSS_SELECTOR, ".alert-success strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_TOTAL_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
