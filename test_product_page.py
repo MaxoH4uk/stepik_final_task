f9b0afeef4b4ddc4292b6dd517727396d9489fa3
maxim <2893_maxon41k@mail.ru>
2021-05-28 09:15:25 +0300

    added test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message and test_message_disappeared_after_adding_product_to_basket tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,42 @@
 from .pages.product_page import ProductPage
+import pytest
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+urls = [
+    '?promo=offer0', '?promo=offer1', '?promo=offer2',
+    '?promo=offer3', '?promo=offer4', '?promo=offer5',
+    '?promo=offer6', pytest.param('?promo=offer7', marks=pytest.mark.xfail),
+    '?promo=offer8', '?promo=offer9'
+]
+
+@pytest.mark.parametrize('url', urls)
+def test_guest_can_add_product_to_basket(browser, url):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{url}"
     page = ProductPage(browser, link)
     page.open()
+    page.should_not_be_success_message()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_message_about_product_added_to_basket()
+    page.should_disappear_message()
     page.should_be_message_basket_total()
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_disappear_message()
