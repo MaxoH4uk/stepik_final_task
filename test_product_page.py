1b7cf52cb69682228b75ab5eca88d62318375320
maxim <2893_maxon41k@mail.ru>
2021-06-01 13:38:28 +0300

    added test_guest_can_go_to_login_page_from_product_page and test_guest_should_see_login_link_on_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 import time
 
@@ -21,21 +22,37 @@ def test_guest_can_add_product_to_basket(browser, url):
     page.should_disappear_message()
     page.should_be_message_basket_total()
 
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    login_page = LoginPage(browser, link)
+    product_page.open()
+    product_page.should_be_login_link()
+    product_page.go_to_login_page()
+    login_page.should_be_login_form()
+
+
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
