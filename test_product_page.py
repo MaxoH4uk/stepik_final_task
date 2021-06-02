284de3fc90c9efcd62a9f695598ce919427d0abb
maxim <2893_maxon41k@mail.ru>
2021-06-02 10:03:18 +0300

    added test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 import pytest
-import time
 
 urls = [
     '?promo=offer0', '?promo=offer1', '?promo=offer2',
@@ -31,6 +30,14 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     product_page.go_to_login_page()
     login_page.should_be_login_form()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_basket_page()
+    product_page.should_be_empty_basket()
+    product_page.should_be_empty_basket_text()
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
