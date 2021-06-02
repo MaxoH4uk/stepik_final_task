c5f3d6a626130fe0b762d37e8227f569755a4c4d
maxim <2893_maxon41k@mail.ru>
2021-06-02 13:40:34 +0300

    deleted basket methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from .locators import BasePageLocators
 from .locators import MainPageLocators
-import math
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=1):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -46,28 +44,9 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    def should_be_basket_button(self):
-        assert self.is_element_present(*MainPageLocators.BASKET_LINK), "Basket link is not presented"
-
-    def should_be_empty_basket(self):
-        assert self.is_not_element_present(*BasePageLocators.PRODUCTS_IN_BASKET), "There are items in the basket"
-
-    def should_be_empty_basket_text(self):
-        assert self.is_element_present(*BasePageLocators.EMPTY_BASKET_TEXT), "Text 'Your basket is empty' is not present"
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
-
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
