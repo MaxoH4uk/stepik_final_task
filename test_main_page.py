b27710f5ec4195ce5a8317cab49afac88503f85c
maxim <2893_maxon41k@mail.ru>
2021-06-02 10:02:54 +0300

    added test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,16 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_basket_button()
+    page.go_to_basket_page()
+    page.should_be_empty_basket()
+    page.should_be_empty_basket_text()
+
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
