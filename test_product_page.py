1302365c3eae6dba4ca097dc6355224dbe30886e
maxim <2893_maxon41k@mail.ru>
2021-06-04 16:03:29 +0300

    created setup fixture

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,22 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+import pytest
+import time
 
-
+@pytest.mark.login
 class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time())
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
@@ -12,13 +25,12 @@ class TestUserAddToBasketFromProductPage():
 
 
     def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
         page.add_to_basket()
         page.should_be_message_about_product_added_to_basket()
-        page.should_disappear_message()
         page.should_be_message_basket_total()
 
 
