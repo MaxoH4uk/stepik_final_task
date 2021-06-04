d8e9add5c8a6c90cb8eedfe6e5c4f5b94e260d02
maxim <2893_maxon41k@mail.ru>
2021-06-04 16:02:11 +0300

    code refactoring

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,7 +7,7 @@ from .locators import MainPageLocators
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=1):
+    def __init__(self, browser, url, timeout=3):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
